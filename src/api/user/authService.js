44ee89064744af49b3e1e0b7a1106c81b610629b
Jefferson Lima <jeffersonlimasantos23@gmail.com>
2018-06-29 23:33:47 -0300

    var

--- src/api/user/authService.js
+++ src/api/user/authService.js
@@ -24,7 +24,7 @@ const login = (req, res, next) => {
             const token = jwt.sign(user, env.authSecret, {
                 expiresIn: "1 day"
             })
-            const { name, email } = user
+            var { name, email } = user
             res.json({ name, email, token })
         } else {
             return res.status(400).send({ errors: ['Usuário/Senha inválidos'] })
